341686cf4fefbe2a2b8505e134faab01ef423d87
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 14:11:26 +0100

    Create _stack.c

---
+++ _stack.c
@@ -0,0 +1,49 @@
+#include "monty.h"
+
+/* Function to push an element onto the stack */
+void push(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to print all elements in the stack */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to print the top element of the stack */
+void pint(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to remove the top element of the stack */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to swap the top two elements of the stack */
+void swap(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to add the top two elements of the stack */
+void add(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to perform a no-op operation */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    /* Your implementation here */
+}
+
+/* Function to free the entire stack */
+void free_stack(stack_t *stack)
+{
+    /* Your implementation here */
+}
