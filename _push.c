94d133b87c7ce7362522ff89b419fe35fd6e3feb
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 14:06:07 +0100

    _push.c

---
+++ _push.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/* Function to push an element onto the stack */
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *arg = strtok(NULL, " \t\n");
+    if (!arg || !is_numeric(arg))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        fclose(file);
+        if (line)
+            free(line);
+        free_stack(stack);
+        exit(EXIT_FAILURE);
+    }
+
+    int value = atoi(arg);
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (!new_node)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        fclose(file);
+        if (line)
+            free(line);
+        free_stack(stack);
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+    if (*stack)
+        (*stack)->prev = new_node;
+    *stack = new_node;
+}
