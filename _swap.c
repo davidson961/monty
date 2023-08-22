b552a8476f8fd7a3864a1242435cb2c32f991263
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 15:16:48 +0100

    Update _swap.c

--- _swap.c
+++ _swap.c
@@ -1,16 +1,22 @@
 #include "monty.h"
 
-/* Function to swap the top two elements of the stack */
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the file.
+ */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        free_stack(*stack);
-        exit(EXIT_FAILURE);
-    }
+	int temp;
 
-    int temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
