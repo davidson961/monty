87a5a4a1f8b10206d4cc615df3a4dcfb3455ea92
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 17:28:59 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -4,9 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-/* Data structure for stack */
+/* Data structure for the stack */
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct stack_s
 {
     int n;
@@ -14,33 +22,37 @@ typedef struct stack_s
     struct stack_s *next;
 } stack_t;
 
-/* Opcode function typedef */
-typedef void (*opcode_func)(stack_t **stack, unsigned int line_number);
-
-/* Instruction structure */
+/* Data structure for the opcode instructions */
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
 typedef struct instruction_s
 {
     char *opcode;
-    opcode_func func;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Function prototypes */
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void div_opcode(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number); // New prototype for mod opcode
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
-int is_numeric(const char *str);
+/* Enumeration for data structure type (STACK or QUEUE) */
+typedef enum data_structure_e
+{
+    STACK,
+    QUEUE
+} data_structure_t;
+
+/* Global variable to store data structure type */
+typedef struct global_s
+{
+    data_structure_t data_structure;
+    /* ... other members ... */
+} global_t;
+
+/* ... other function prototypes ... */
+
+extern global_t global;
 
 #endif /* MONTY_H */
