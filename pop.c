73abf9b9a967053ef4778ef9535eedac8f748ca0
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 14:31:52 +0100

    Update pop.c

--- pop.c
+++ pop.c
@@ -6,10 +6,7 @@ void pop(stack_t **stack, unsigned int line_number)
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        fclose(file);
-        if (line)
-            free(line);
-        free_stack(stack);
+        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
 
