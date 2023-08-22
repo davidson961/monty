c1086fdce850e593463d082423843601cd3cd39b
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 17:17:22 +0100

    Create pstr.c

---
+++ pstr.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * pstr - Prints the string starting at the top of the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the file.
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    while (current != NULL && current->n != 0 && current->n >= 0 && current->n <= 127)
+    {
+        printf("%c", current->n);
+        current = current->next;
+    }
+
+    printf("\n");
+    (void)line_number; // Unused parameter
+}
