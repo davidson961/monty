e56a524625c13e50a35d22acdf46dba8dff44daf
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 17:18:57 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -37,6 +37,7 @@ void div_opcode(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number); // New prototype for mod opcode
 void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 int is_numeric(const char *str);
 
