57b0d177c449f3a6bfaf75a2f8a14283b4964b6c
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 20:21:01 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdbool.h>
 
 #define STACK 0
@@ -13,7 +18,23 @@ typedef struct stack_s
 {
     int n;
     struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
+
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
 /* Global variable to track data structure (stack or queue) */
 extern struct monty_global_s
@@ -22,6 +43,9 @@ extern struct monty_global_s
 } global;
 
 /* Opcode function prototypes */
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
@@ -44,4 +68,4 @@ void queue(stack_t **stack, unsigned int line_number);
 int is_numeric(const char *str);
 void free_stack(stack_t *stack);
 
-#endif /* MONTY_H */
+#endif
