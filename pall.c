10a359ab88214539100dca3643b8b35c628563db
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 15:22:02 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,12 +1,19 @@
 #include "monty.h"
 
-/* Function to print all elements in the stack */
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Double pointer to the top of the stack.
+ * @line_number: Line number in the file.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    while (current)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	stack_t *current = *stack;
+
+	(void)line_number;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
