fc157bd69eafccc4d21b7b057906e25622e2261c
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 15:57:55 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -33,7 +33,7 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
-void div(stack_t **stack, unsigned int line_number); // New prototype for div opcode
+void div_opcode(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 int is_numeric(const char *str);
 
