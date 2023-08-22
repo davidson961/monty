44edac33531016a7fe18e74064bf0225f7e77fa4
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 14:21:32 +0100

    pop.c

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/* Function to remove the top element of the stack */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        fclose(file);
+        if (line)
+            free(line);
+        free_stack(stack);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack)
+        (*stack)->prev = NULL;
+    free(temp);
+}
