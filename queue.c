86f79eddca61446334bf85e5bcafa961043e4014
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-22 17:52:51 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 
 /**
- * queue - Sets the format of the data to a queue (FIFO).
- * @stack: Double pointer to the top of the stack.
- * @line_number: Line number in the file.
+ * queue - Set the format of the data to a queue (FIFO)
+ * @stack: Double pointer to the head of the stack
+ * @line_number: Line number being executed from the Monty file
  */
 void queue(stack_t **stack, unsigned int line_number)
 {
