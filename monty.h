cf7a22e76bf999c8e8093ba399df47604b60b2da
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-23 16:43:15 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -64,5 +64,6 @@ void rotr(stack_t **stack, unsigned int line_number);
 void stack(stack_t **stack, unsigned int line_number);
 void queue(stack_t **stack, unsigned int line_number);
 int is_numeric(const char *str);
+void free_stack(stack_t *stack);
 
 #endif
