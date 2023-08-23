3620eef20d2ffe9494f2de2a36d0d8abb9b0fa3a
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-23 13:52:51 +0100

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -3,8 +3,9 @@
 /**
  * rotr - Rotates the stack to the bottom.
  * @stack: Double pointer to the top of the stack.
+ *@line_number: line_number
  */
-void rotr(stack_t **stack, unsigned int line_number)
+void rotr(stack_t **head, __attribute__((unused)) unsigned int line_number)
 {
     stack_t *last = *stack;
     stack_t *secondLast = *stack;
