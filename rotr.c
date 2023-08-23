27224a814fcc23eac4a6e9aa8822ad8b8c436ed7
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-08-23 13:56:27 +0100

    Update rotr.c

--- rotr.c
+++ rotr.c
@@ -1,25 +1,26 @@
 #include "monty.h"
 
 /**
- * rotr - Rotates the stack to the bottom.
+ * rotr - Rotates the stack to the bottom (right).
  * @stack: Double pointer to the top of the stack.
- *@line_number: line_number
+ * @line_number: Line number in the file.
  */
-void rotr(stack_t **head, __attribute__((unused)) unsigned int line_number)
+void rotr(stack_t **stack, unsigned int line_number)
 {
+    stack_t *current = *stack;
     stack_t *last = *stack;
-    stack_t *secondLast = *stack;
 
     if (*stack == NULL || (*stack)->next == NULL)
         return;
 
     while (last->next != NULL)
-    {
-        secondLast = last;
         last = last->next;
-    }
 
-    secondLast->next = NULL;
+    last->prev->next = NULL;
+    last->prev = NULL;
     last->next = *stack;
+    (*stack)->prev = last;
     *stack = last;
+
+    (void)line_number; 
 }
