14878854cdebfa40b5f01bf28df70acd11feea39
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-10-14 01:59:12 +0000

    Update set_op_toks_error.c

--- set_op_toks_error.c
+++ set_op_toks_error.c
@@ -1,9 +1,8 @@
-
 #include "monty.h"
 
 /**
- * set_op_tok_error - Sets last element of op_toks to be an error code.
- * @error_code: Integer to store as a string in op_toks.
+ * set_op_tok_error - Assigns an error code as the last element in op_toks.
+ * @error_code: Integer to be stored as a string in op_toks.
  */
 void set_op_tok_error(int error_code)
 {
