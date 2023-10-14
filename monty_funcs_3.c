82d6361f204afad4b141be60de77e57d7febc84d
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-10-14 01:44:33 +0000

    Update monty_funcs_3.c

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -1,15 +1,9 @@
-
-
 #include "monty.h"
 
-void monty_nop(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
-void monty_pstr(stack_t **stack, unsigned int line_number);
-
 /**
- * monty_nop - Does absolutely nothing for the Monty opcode 'nop'.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
+ * monty_nop - Performs no operation for the Monty opcode 'nop'.
+ * @stack: A pointer to the topmost node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecode file.
  */
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
@@ -18,10 +12,9 @@ void monty_nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * monty_pchar - Prints the character in the top value
- *               node of a stack_t linked list.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
+ * monty_pchar - Prints the character in the top element of a stack_t linked list.
+ * @stack: A pointer to the topmost node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecode file.
  */
 void monty_pchar(stack_t **stack, unsigned int line_number)
 {
@@ -42,8 +35,8 @@ void monty_pchar(stack_t **stack, unsigned int line_number)
 
 /**
  * monty_pstr - Prints the string contained in a stack_t linked list.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
+ * @stack: A pointer to the topmost node of a stack_t linked list.
+ * @line_number: The current line number in the Monty bytecode file.
  */
 void monty_pstr(stack_t **stack, unsigned int line_number)
 {
