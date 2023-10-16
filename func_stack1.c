ff8880cf5f2c90695b01d6585fba07c7b5403680
davidson961 <125483143+davidson961@users.noreply.github.com>
2023-10-16 01:30:30 +0000

    Update func_stack1.c

--- func_stack1.c
+++ func_stack1.c
@@ -26,6 +26,33 @@ void add_to_stack(stack_t **recent_node,
 
 
 /**
+ * add_to_queue - Inserts a node into the queue.
+ * @recent_node: The pointer to the new node.
+ * @line_num: The line number of the opcode.
+ */
+void add_to_queue(stack_t **recent_node,
+		__attribute__((unused))unsigned int line_num)
+{
+	stack_t *temp;
+
+	if (recent_node == NULL || *recent_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *recent_node;
+		return;
+	}
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = *recent_node;
+	(*recent_node)->prev = temp;
+
+}
+
+
+/**
  * print_stack - Displays the contents of a stack.
  * @stack: The pointer to a pointer pointing to the top node of the stack.
  * @line_num: The line number of the opcode.
@@ -44,7 +71,6 @@ void print_stack(stack_t **stack, unsigned int line_num)
 		temp = temp->next;
 	}
 }
-
 
 
 /**
